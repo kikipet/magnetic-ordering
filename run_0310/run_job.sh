17230a064c3de1828e34d5fd84a36ce7bfdace9c
kikipet <songk@mit.edu>
2022-03-10 09:02:47 -0500

    update supercloud branch, make code runnable via jobs

---
+++ run_0310/run_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /etc/profile
+cd /home/gridsan/sekim
+module load anaconda/2022a
+eval "$(conda shell.bash hook)"
+source .bashrc
+conda activate /home/gridsan/sekim/.conda/envs/e3nn
+
+cd magnetic-ordering/run_0310
+python data_preprocess_no_mendeleev.py
+python magnetic_ordering_draft.py
---
+++ run_0310/run_model.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -c 5
+#SBATCH --gres=gpu:volta:1
+
+source /etc/profile
+cd /home/gridsan/sekim
+module load anaconda/2022a
+eval "$(conda shell.bash hook)"
+source .bashrc
+conda activate /home/gridsan/sekim/.conda/envs/e3nn
+
+cd magnetic-ordering/run_0310
+python magnetic_ordering_draft.py
